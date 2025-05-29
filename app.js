cf8d5d226f5a26b007796e917c4515af19457242
Delight-Mpashi <delight_mpashi@outlook.com>
2025-05-29 22:06:39 +0200

    changes to the path file

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Serve static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Public')));
 app.use('/components', express.static(path.join(__dirname, 'src/components')));
 app.use('/pages', express.static(path.join(__dirname, 'src/pages')));
 app.use('/src-assets', express.static(path.join(__dirname, 'src/assets')));
 
 // Root route (only sends index.html)
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'Public', 'index.html'));
 });
 
 app.listen(port, () => {
