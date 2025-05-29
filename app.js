0d97f24e4ac60e48a4ababcb64dac7ee54af252e
Delight-Mpashi <delight_mpashi@outlook.com>
2025-05-29 21:58:49 +0200

    update on the app.js file

--- app.js
+++ app.js
@@ -3,17 +3,15 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve all of /public statically
+// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Serve extra directories
 app.use('/components', express.static(path.join(__dirname, 'src/components')));
 app.use('/pages', express.static(path.join(__dirname, 'src/pages')));
 app.use('/src-assets', express.static(path.join(__dirname, 'src/assets')));
 
-// Root route
+// Root route (only sends index.html)
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src','components', 'public', 'index.html', 'navbar.html','footer.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
