f80be7c32d042ece0f6a1425184f7f4dfdcb6e78
Delight-Mpashi <delight_mpashi@outlook.com>
2025-05-29 18:16:18 +0200

    this is the first commit with only front-end and no backend
    this is the first commit with only front-end and no backend

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// Static folder path
+const publicDir = path.join(__dirname, '..', 'SpectrumWebsite-New', 'Public');
+app.use(express.static(publicDir));
+
+// Route to serve index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(publicDir, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
