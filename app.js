58a4b3134a09754bba280d6acf7131578478bd25
Delight-Mpashi <delight_mpashi@outlook.com>
2025-05-29 21:07:46 +0200

    fixed path in the app.js file

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
 const port = process.env.PORT || 3000;
 
-
+// Serve all of /public statically
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Serve extra directories
+app.use('/components', express.static(path.join(__dirname, 'src/components')));
+app.use('/pages', express.static(path.join(__dirname, 'src/pages')));
+app.use('/src-assets', express.static(path.join(__dirname, 'src/assets')));
 
-app.use('/Public/assets/Images', express.static(path.join(__dirname, 'public/assets/Images')));
-app.use('/Public/assets/Fonts', express.static(path.join(__dirname, 'public/assets/Fonts')));
-app.use('/Public/assets/Styles', express.static(path.join(__dirname, 'public/assets/Styles')));
-app.use('/src/components', express.static(path.join(__dirname, 'src/components')));
-app.use('/src/pages' , express.static(path.join(__dirname, 'src/pages')));
-app.use('/src/assets', express.static(path.join(__dirname, 'src/assets')));
-app.use('/public/assets', express.static(path.join(__dirname, 'public/assets')));
-
-
-app.use('/src', express.static(path.join(__dirname, 'src')));
-
-
+// Root route
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
