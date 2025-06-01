1ef2926faa579821566c6a1ae5a9a9fdc5aa08e5
Delight-Mpashi <delight_mpashi@outlook.com>
2025-06-01 22:57:43 +0200

    path adjustments to see if I can render images when node is deployed on render

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 // Serve static files from Public directory
-app.use(express.static(path.join(__dirname, 'Public'), {
+app.use(express.static(path.join(__dirname, 'public'), {
   // Additional static file options
   maxAge: '1d', // Cache static assets for 1 day
   setHeaders: (res, path) => {
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, 'Public'), {
 
 // Root route
 app.get('/', (req, res) => {
-  const indexPath = path.join(__dirname, 'Public', 'index.html');
+  const indexPath = path.join(__dirname, 'public', 'index.html');
   
   // Verify file exists before sending
   if (fs.existsSync(indexPath)) {
@@ -29,7 +29,7 @@ app.get('/', (req, res) => {
 
 // Test route for image debugging
 app.get('/test-image', (req, res) => {
-  const imagePath = path.join(__dirname, 'Public', 'assets', 'images', 'dashboardimage.jpg');
+  const imagePath = path.join(__dirname, 'public', 'assets', 'images', 'dashboardimage.jpg');
   
   if (fs.existsSync(imagePath)) {
     res.json({
@@ -54,7 +54,7 @@ app.use((req, res) => {
 // Error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).sendFile(path.join(__dirname, 'Public', '500.html'));
+  res.status(500).sendFile(path.join(__dirname, 'public', '500.html'));
 });
 
 app.listen(port, () => {
